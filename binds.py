8a74a727619381a78961b24dfe4edda25ec07edc
agesoi <mikhail-borisov-2024@internet.ru>
2024-10-16 13:43:30 +0300

    v0.4
    Ну добавили иконку и файл json

--- binds.py
+++ binds.py
@@ -4,11 +4,11 @@ import keyboard
 
 #быстрое объяснение в цикле мы ожидаем когда пользователь нажмёт кнопку что бы исполнить файл 
 
-        
+   
 def press_keyboard():
     while True:
         keyboard.wait("9")
         subprocess.Popen(r"C:\Users\agesoi\AppData\Roaming\Telegram Desktop\Telegram.exe")
 
 
-
+press_keyboard()
--- window.py
+++ window.py
@@ -2,14 +2,13 @@ from tkinter import *
 from tkinter import ttk
 
 
+
+
 window = Tk()
-window.geometry('500x500')
+window.geometry('800x750+400+200')
+window.iconbitmap(default="icon.ico")
+window.title('programs_bind')
 
-def night_theme():
-    window['bg'] = '#000000'
-    
 
-btn = ttk.Button(text='night theme', width=20, command=night_theme)
-btn.pack(expand=True, ipadx=10, ipady=10)
 
 window.mainloop()
