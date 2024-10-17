1e11e9e825f76afa71febd7f35c847d91df590ca
agesoi <mikhail-borisov-2024@internet.ru>
2024-10-17 17:16:26 +0300

    v0.5

--- window.py
+++ window.py
@@ -5,10 +5,28 @@ from tkinter import ttk
 
 
 window = Tk()
-window.geometry('800x750+400+200')
+window.geometry('500x500')
+window.resizable(False, False)
 window.iconbitmap(default="icon.ico")
 window.title('programs_bind')
 
+
+def select_file_path():
+    label = ttk.Label(text="select file path:")
+    label.pack(anchor=W, padx=6, pady=6)
+
+    entry = ttk.Entry()
+    entry.pack(anchor=W, padx=6, pady=6)
+    
+    global path_entry
+    path_entry = entry.get()
+    
+    btn = ttk.Button(text="Save")
+    btn.pack(anchor=W, padx=6, pady=6)
+    
+    
+btn = ttk.Button(text="Create bind", command=select_file_path)
+btn.pack(anchor=N, padx=6, pady=6)
 
 
 window.mainloop()
